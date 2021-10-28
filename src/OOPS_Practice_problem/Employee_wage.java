6561aa2b04b609ba1fa6b300cf7dc50fe20a0ed8
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-28 16:37:53 +0530

    Javaprogram

--- src/OOPS_Practice_problem/Employee_wage.java
+++ src/OOPS_Practice_problem/Employee_wage.java
@@ -10,9 +10,15 @@ public static void main(String[] args) {
 	int attendance_check = (int)(Math.random()*10)%2;
 	if (attendance_check == 1) {
 		System.out.println("Employee is Present");
+		employee_daily_wage();
 	}
 	else {
 		System.out.println("Employee is Absent");	
 	}
   }
+     static void employee_daily_wage() {
+    	 int wage_per_hrs = 20;
+    	 int working_hrs = 8;
+    	 System.out.println("Employee daily wages is "+wage_per_hrs*working_hrs);
+     }
 }
